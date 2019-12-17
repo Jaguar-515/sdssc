77d1c9c6da2105cba3c6b7f542b6ff5214a7caef
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-12-16 21:28:48 -0800

    Shorten Constitution link
    TIL that TinyURL has a neat little preview feature that allows people to view the link they are about to go to.
    However, for that to work you need to enable cookies in your browser...

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ sub_size = 3741
 def get_cube_root(num):
     return num ** (1. / 3)
 
-# The equation. This can be found in Appendix 1.3 of the SimDemocracy Constitution: https://docs.google.com/document/d/1x3EwwglsRgnBRDhJOB8DcSZHG7ZLahG_enitXqqvDWA/edit#heading=h.jj8r0y1g96nf
+# The equation. This can be found in Appendix 1.3 of the SimDemocracy Constitution: https://preview.tinyurl.com/t6bacb7
+
 equation =  round(1.25 * (get_cube_root(2 * presidental_turnout + senate_turnout + census_response_rate + 0.1 * sub_size)), 0)
 
 # Now show the equation
