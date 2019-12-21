99194cc922d523b9bb5ecd92c28ba17f5564ec24
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-12-20 18:06:38 -0800

    Rewrite equation
    https://docs.google.com/document/u/0/d/11-oYYVBlSUfccjGCdcIF5h14DQzcBxbM6rIgUgWbc8M/mobilebasic
    This depends on if it passes or not. If it does, then I'll merge this to master.

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
+import math
 # Variables
-presidental_turnout = 16
 senate_turnout = 26
-census_response_rate = 100
-sub_size = 3741
+votes = 132
 
-# Cube root function since python's math module doesn't include one
-def get_cube_root(num):
-    return num ** (1. / 3)
-
-# The equation. This can be found in Appendix 1.3 of the SimDemocracy Constitution: https://preview.tinyurl.com/t6bacb7
-
-equation =  round(1.25 * (get_cube_root(2 * presidental_turnout + senate_turnout + census_response_rate + 0.1 * sub_size)), 0)
+# Equation
+equation =  round(math.sqrt(senate_turnout + votes / 2))
 
 # Now show the equation
 print(equation)
